7ae86e14ce9071c1b38b71c467fc38efda688d4d
Drew Tucker and Ernest Bruno <ernest@>
2018-01-16 14:42:40 -0800

    Change logos

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,14 @@
 $(document).ready(function(){
-  var suits = [" of Clubs", " of Diamonds", " of Spades", " of Hearts"];
-  var numbers = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"];
+  // Suits [Clubs, Diamonds, Spades, Hearts]
+  var suits = [" of &#9827;", " of &#9826;", " of &#9824;", " of &#9825;"];
+  var numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
   suits.forEach(function(suit){
-  numbers.forEach(function(number){
-      $("#deck").append("<li>" + number + suit + "</li>");
+    numbers.forEach(function(number){
+      if(number == "A" || number == "J" || number == "Q" || number == "K"){
+        $("#deck").append("<li> <strong>" + number + "</strong>" + suit + " </li>");
+      } else {
+        $("#deck").append("<li>" + number + suit + "</li>");
+      };
     });
   });
 
